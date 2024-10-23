bff9c9ec0dd878d06b76d03e38b4798115756729
something-natural <tsglow@naver.com>
2024-10-23 09:45:44 +0900

    fix: change functions

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
+/*
+1. default logic
+    when event 'onload' happes, 
+    a. make div(class label) element
+    b. using charRange(), foreach label div from a to j as 'letters'
+    c. using range, make array 'number' from 1 to 99
+
+
+*/
+
+
+//function range to make array from start to end numbers
 const range = (start, end) => Array(end - start + 1)
     .fill(start)
     .map((el,index) => el + index);
 
+//function charRange to make array from start to end letters
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0))
     .map(el => String.fromCharCode(el))
 
 
-const update = (event) => console.log(event.target.value);
-
-window.onload = () => {
-    const container = document.getElementById("container");    
-    const createLabel = letter => {
+//function make div with class 'label' and append to parent
+const makeLabel = (el,parent) => {    
         const label = document.createElement("div");
         label.className = "label";
-        label.textContent = letter;
-        container.appendChild(label);
-    }
-           
-    const letters = charRange("A", "J");
-    letters.forEach(createLabel);
+        label.textContent = el;
+        parent.appendChild(label);    
+}
+
+//function make input with id 'letter + number" and append to parent, with property 'onchange = update'
+const makeInput = (letter, number, parent) => {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.id = letter + number;
+    input.ariaLabe = letter + number;
+    parent.appendChild(input);
+    input.onchange = update;
+}
+
+//function update to get input value and parse
+const update = (event) => {
+    console.log(event.target.value);
+}
+
+
+window.onload = () => {
+    //make container as sheet
+    const container = document.getElementById("container");                       
     
+    //make div from A -J array and append to container
+    const letters = charRange("A", "J");
+    letters.forEach((letter) => makeLabel(letter, container));    
+    
+    //make numbers array
     const numbers = range(1,99);
     numbers.forEach((number) => {
-        createLabel(number);
-        letters.forEach(letter => {
-            const input = document.createElement("input");
-            input.type = "text";
-            input.id = letter + number;
-            input.ariaLabe = letter + number;
-            container.appendChild(input);
-            input.onchange = update;
-        })
+        // using numbers, make 1-99 div.
+        // so now structure is <div class = "label">J</div><div class = "label">1</div>...<div class = "label">1</div>
+        makeLabel(number, container);
+        
+        // now you're using numbers.foreEach, after below, structure is
+        // <div class = "label">J</div><div class = "label">1</div><input id = a1></input><input id = J1></input><div class = "label">2</div>...
+        letters.forEach((letter) => makeInput(letter, number, container));
     })    
 }
